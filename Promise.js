72a93a7031a81476391381739abe188ffc62d076
AnjaliRaiDaffodilsw <anjali.rai@unthinkable.co>
2021-05-06 19:03:20 +0530

    added more examples

--- Promise.js
+++ Promise.js
@@ -86,24 +86,86 @@
 //     console.table(error);
 // })
 
+
 //Promise - eg 5 
 
 
-const promise = new Promise((resolve, reject) => {
-    const x = "all the best";
-    const y = "all the best";
-    if(x === y) {
-      resolve();
-    } else {
-      reject();
-    }
-  });
+// const promise = new Promise((resolve, reject) => {
+//     const x = "all the best";
+//     const y = "all the best";
+//     if(x === y) {
+//       resolve();
+//     } else {
+//       reject();
+//     }
+//   });
     
-  promise.
-      then( () => {
-          console.log('Success, You are a Genius🙋');  
-      }).
-      catch( () => {
-          console.log('Some error has occured');
-      });
+//   promise.
+//       then( () => {
+//           console.log('Success, You are a Genius🙋');  
+//       }).
+//       catch( () => {
+//           console.log('Some error has occured');
+//       });
   
+//Promise using async-await eg 6
+
+// const pro2 = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             let roll_no = ["anjali","vidhya","neetu","pramod"];
+//             resolve(roll_no);
+//             reject('Error while communicating');
+//         }, 2000);
+//     });
+    
+//     const getbioData = (name) => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout((name) => {
+//                 let biodata = {
+//                     office : "ABC pvt ltd",
+//                     age: 23
+//                 }
+//                 resolve(`My name is ${name} , I am ${biodata.age} years old and my office is  ${biodata.office}`);
+//                 reject(`Your details are not correct`);
+//             }, 2000, name);
+//         })
+//     }
+    
+//     //promise consumption
+//     async function getData () {
+//         const namedata = await pro2;
+//         console.log(namedata);
+//         const biodatas = await getbioData(namedata[0]);
+//         console.log(biodatas);
+
+//         return biodatas;
+//     }
+//     const data = getData().then((getname) => {
+//         console.log(getname)
+//     });
+
+
+    //Promise 7 - 
+
+    // const promise = new Promise((resolve, reject) => {
+    //     setTimeout(() =>  {
+    //         resolve('hello JS');
+    //         reject("something is wrong")
+    //     }, 2000);
+    // });
+    
+    // promise.then((data) => {
+    //     console.log(data + ' 1');
+    // });
+    
+    // promise.then((data) => {
+    //     console.log(data + ' 2');
+    // });
+    
+    // promise.then((data) => {
+    //     console.log(data + ' 3');
+    // });
+
+    // promise.catch ((error) => {
+    //     console.log(error);
+    // })
